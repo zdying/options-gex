76f66075567af59df218d095e21f718d46a395f3
zhangdaiying <zhangdaiying@xiaomi.com>
2026-06-29 12:48:36 +0800

    新增无风险利率等参数配置

---
+++ src/config/pricingConfig.js
@@ -0,0 +1,11 @@
+const pricingConfig = {
+  riskFreeRate: 0.0383,
+  dividendYieldByTicker: {
+    SPY: 0.0103,
+    QQQ: 0.0040,
+    MU: 0.0010
+  },
+  minRealtimeTMinutes: 5
+};
+
+module.exports = pricingConfig;
